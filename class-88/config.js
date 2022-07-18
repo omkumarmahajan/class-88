e7fa7554072e4b8b0e6d73209246903bf6173c28
omkumarmahajan <77659322+omkumarmahajan@users.noreply.github.com>
2022-07-18 17:44:21 +0530

    Add files via upload

---
+++ class-88/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA5PcN4FmK3FPKpASdUASu2m-AJOwaVoEU",
+  authDomain: "class-85-bc666.firebaseapp.com",
+  databaseURL: "https://class-85-bc666-default-rtdb.firebaseio.com",
+  projectId: "class-85-bc666",
+  storageBucket: "class-85-bc666.appspot.com",
+  messagingSenderId: "190923809108",
+  appId: "1:190923809108:web:38078072f1bd46c99aa8c1",
+  measurementId: "G-Z6MN74YLW7"
+  };
